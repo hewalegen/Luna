fd0e69ce987db752851a6d3bc7c4f3062a554df8
NechiR22 <81181011+NechiR22@users.noreply.github.com>
2021-04-10 19:23:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODMwNDEwOTc1MjIzMDg3MTU2.YHGSfA.lmBc20WRPv7VQbskk5rI8pHQZkk');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
