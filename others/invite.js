2d570cf3064d36d4378280323a6f6dc79bc6387b
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-20 14:24:52 +0300

    Create invite.js

---
+++ others/invite.js
@@ -0,0 +1,21 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const {
+  PREFIX,
+  approveemoji,
+  denyemoji
+} = require(`../config.json`);
+
+module.exports = {
+  name: `invite`,
+  description: `Gives you an invite`,
+  aliases: ["add"],
+  cooldown: 3,
+  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
+  execute(message, args, client) {
+    //react with approve emoji
+    message.react("✅");
+    //send the invite embed
+    message.reply(new MessageEmbed().setColor("#FF0000").setTitle("❤ Please Invite me: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=844069682545164289&permissions=8&scope=bot")
+    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/844069682545164289/b40e8a61cbeb7a6319b5050238a9aa03.png?size=1024"));
+  }
+}
