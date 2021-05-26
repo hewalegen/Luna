9edbb88d8464ddd4d0f4029edd49ee8df8366db0
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-26 15:00:18 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,21 +18,19 @@ module.exports = {
     .setThumbnail(`https://cdn.discordapp.com/avatars/844069682545164289/b40e8a61cbeb7a6319b5050238a9aa03.png?size=1024`)
     .setTitle(`**Ness Help**`)
     .setDescription(`
-┈───────┄✨┄───────┈
-~~»User Commands~~
+
+**User Commands**
 > \`n!invite\` - \`n!support\` - \`n!about\`
 > \`n!ping\` - \`n!prefix\` - \`n!uptime\`
 > \`n!se\` - \`n!avatar\` - \`n!embed\`
 > \`n!invites\`
-┈───────┄🎶┄───────┈
-~~»Music Commands~~
+**Music Commands**
 > \`n!play\` - \`n!skip\` - \`n!skipto\`
 > \`n!stop\` - \`n!volume\` - \`n!nowplaying\`
 > \`n!shuffle\` - \`n!search\` - \`n!resume\`
 > \`n!remove\` - \`n!queue\` - \`n!filter\`
 > \`n!loop\` - \`n!lyrics\` - \`n!radio\`
-┈───────┄😃┄───────┈
-~~»Fun Commands~~
+**Fun Commands**
 > \`n!lock\` - \`n!unlock\`
 > \`n!ban\` - \`n!unban\`
 > \`n!mute\` - \`n!unmute\`
@@ -40,13 +38,11 @@ module.exports = {
 > \`n!clear\` - \`n!say\`
 > \`n!bans\` - \`n!cv\`
 > \`n!slap\` - \`n!hug\`
-┈───────┄🧸┄───────┈
-~~»Gifs Commands~~
+**Gifs Commands**
 > \`n!boy\` - \`n!girl\` - \`n!baby\`
 > \`n!couple\` - \`n!animal\` - \`n!anime\`
 > \`n!emoji\`
-┈───────┄🖇️┄───────┈
-~~»Links~~
+**Links**
 **[Support](https://discord.gg/2jAP99jssR) - [invite](https://discord.com/api/oauth2/authorize?client_id=844069682545164289&permissions=8&scope=bot)**
 `)
 
