b83975526b559fc061c8fd8488b2c1072ef8f7df
NechiR22 <81181011+NechiR22@users.noreply.github.com>
2021-05-26 17:44:46 +0300

    Update emojis.js

--- emojis.js
+++ emojis.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const {MessageEmbed} = require('discord.js');
 
 module.exports={
-    name: 'emojis',
+    name: 'emojilist',
     category: 'guild',
     description: 'displays every emoji of the guild, where the command is used',
     aliases: ['emojilist'],
