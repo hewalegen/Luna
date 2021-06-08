4162546f88978bf0b0013e91dec332f9b2315044
Wolf721 <84997879+Wolf-721@users.noreply.github.com>
2021-06-09 00:00:58 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,7 +15,7 @@ module.exports = {
     //react with approve emoji
     message.react("<:emoji_15:830469967752724500>");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#FFF712").setTitle("❤ Please Invite me: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=844069682545164289&permissions=8&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/844069682545164289/b40e8a61cbeb7a6319b5050238a9aa03.png?size=1024"));
+    message.reply(new MessageEmbed().setColor("#FFF712").setTitle("❤ Please Invite me: ").setDescription("https://discord.com/api/oauth2/authorize?client_id=834488236721831957&permissions=8&scope=bot")
+    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/attachments/850352717511000065/851928296093909002/icons.jpg"));
   }
 }
